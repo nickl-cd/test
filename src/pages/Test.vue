e55eb8731d6a1bda97ba3a490f69b446be6db847
Nick <nicholas.lal@canadadrives.ca>
2022-01-25 10:00:52 -0800

    -setup test page

---
+++ src/pages/Test.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="about">
+    <h1>This is a test page</h1>
+    <input
+      v-model="inputValue"
+      @input="showDelayedSearchResults(inputValue)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+
+  data () {
+    inputValue: "",
+    cities: [
+      "Aberdeen",
+      "Abilene",
+      "Akron",
+      "Albany",
+      "Albuquerque",
+      "Alexandria",
+      "Allentown",
+      "Amarillo",
+      "Anaheim",
+      "Anchorage",
+      "Ann Arbor",
+      "Antioch",
+      "Apple Valley",
+      "Appleton",
+      "Arlington",
+      "Arvada",
+      "Asheville",
+      "Lacey",
+      "Lafayette",
+      "Lake Charles"
+    ]
+  },
+
+  mounted () {
+    console.log(this.cities)
+  }
+
+  methods: {
+    showDelayedSearchResults (input) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          return this.cities.filter(city => city.toLowerCase() === input.toLowerCase())
+        }, 1000)
+      )
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "../pages/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -11,17 +11,18 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/test",
+    name: "Test",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../pages/Test.vue"),
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
 });
 
